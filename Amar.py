c44e7e6de6c50307f88038c47047cbb736df9ee3
Amuigduek <93607057+Amuigduek@users.noreply.github.com>
2021-11-12 21:50:01 +0530

    Add files via upload

---
+++ Amar.py
@@ -0,0 +1,26 @@
+a="""A television or radio broadcast that gives you information about recent events, read by a newsreader with special reports by correspondents. ... This shows that news can come from anywhere. News is the report of a current event, something that was not known, information of recent events and happenings"""
+print(len(a))
+print(a.splitlines(True))
+print(a.title())
+print(type(a))
+
+
+print(a)
+a=5
+b=5
+print(a+b)
+para=[]
+print(type(para))
+para=[65,85,95]
+print(para[2])
+para=[65,85,95]
+print(para)
+para=["65","85","95"]
+print('|'.join(para))
+
+#comparison operator or relation operator
+a=10
+b=15
+print(a==b)
+print(a<b)
+print(a>b)
